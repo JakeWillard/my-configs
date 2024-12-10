c34d6b1fe259f29b3c5a62195e759712c4f10114
JakeWillard <jacob.willard.gr@dartmouth.edu>
2024-12-09 19:04:34 -0500

    :)

--- common.nix
+++ common.nix
@@ -12,19 +12,9 @@
   gnome-sys.scheme = "marrakesh";
 
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  # boot.loader.grub.enable = true;
-  # boot.loader.grub.device = "/dev/sda";
-  # boot.loader.grub.useOSProber = true;
-
   # alias for updating the flake
   programs.bash.shellAliases.update = "sudo nix flake update --flake ~/my-configs";
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
   networking.networkmanager.enable = true;
@@ -59,16 +49,7 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.jake = {
@@ -105,25 +86,6 @@
     popsicle
     
   ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
--- hosts/pulseaudio.nix
+++
@@ -1,8 +0,0 @@
-{ config, lib, pkgs, modulesPath, ... }:
-
-{
-  hardware.pulseaudio.configFile = pkgs.runCommand "default.pa" {} ''
-  sed 's/module-udev-detect$/module-udev-detect tsched=0/' \
-    ${pkgs.pulseaudio}/etc/pulse/default.pa > $out
-  '';
-}
