cd51d13b599f34d2a689a7e116ab7ce5a906f81b
JakeWillard <jacob.willard.gr@dartmouth.edu>
2026-03-01 17:44:15 -0500

    :D

--- hosts/nvidia.nix
+++ hosts/nvidia.nix
@@ -10,7 +10,7 @@
 
   hardware.nvidia = {
     
-    modesetting.enable = false;
+    modesetting.enable = true;
     powerManagement.enable = false;
     powerManagement.finegrained = false;
     open = false;
